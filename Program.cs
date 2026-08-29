65d01997b2a8680b54d62f36cf42b7d4c245b084
saletty <salet.ortega03@gmail.com>
2026-08-28 22:36:07 -0400

    fix: vincular app al puerto y host publico 0.0.0.0 para Render

--- Program.cs
+++ Program.cs
@@ -6,14 +6,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. SOLUCIÓN ERROR INOTIFY EN RENDER (Deshabilitar reloadOnChange)
+// 1. VINCULAR PUERTO DE RENDER (0.0.0.0)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+// 2. SOLUCIÓN ERROR INOTIFY EN RENDER (Deshabilitar reloadOnChange)
 builder.Configuration.Sources.Clear();
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false)
     .AddEnvironmentVariables();
 
-// 2. CONFIGURACIÓN DE LÍMITES (100MB para Videos)
+// 3. CONFIGURACIÓN DE LÍMITES (100MB para Videos)
 builder.Services.Configure<KestrelServerOptions>(options =>
 {
     options.Limits.MaxRequestBodySize = 100 * 1024 * 1024; // 100MB
@@ -24,18 +28,18 @@ builder.Services.Configure<FormOptions>(options =>
     options.MultipartBodyLengthLimit = 100 * 1024 * 1024; // 100MB
 });
 
-// 3. CLOUDINARY
+// 4. CLOUDINARY
 builder.Services.AddSingleton(new Cloudinary(new Account(
     builder.Configuration["Cloudinary:CloudName"],
     builder.Configuration["Cloudinary:ApiKey"],
     builder.Configuration["Cloudinary:ApiSecret"]
 )));
 
-// 4. Base de Datos PostgreSQL
+// 5. Base de Datos PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 5. CORS
+// 6. CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirReact", policy =>
@@ -55,7 +59,9 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+//  para evitar advertencias de puerto en Render
+// app.UseHttpsRedirection();
+
 app.UseCors("PermitirReact");
 app.UseAuthorization();
 
